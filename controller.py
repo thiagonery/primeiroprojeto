ce2a8faa1fde4fd2eb3d15b598eae791939de572
thiagonery <60719065+thiagonery@users.noreply.github.com>
2020-04-29 22:07:53 -0300

    Update controller.py
    aula 29/04

--- controller.py
+++ controller.py
@@ -5,6 +5,23 @@ from aplicacao import app
 @app.route('/home')
 
 def ola():
-        return render_template('index.html')
+        retorno = render_template(
+                'index.html', 
+                title='PaginaFlask',
+                outra = 'novo texto',
+                lista = ['a','b','c']
+                )
+
+        return retorno
+
+@app.route('/')
+
+def pagina_principal():
+        return '<h1> pagina principal </h1><p> ola </p>'
+
+@app.route('/outrapagina')
+
+def outra_pagina():
+        return '<h1> Outra pagina </h1>'        
 
 app.run(debug=True)
